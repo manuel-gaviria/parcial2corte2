88ac33580918360f41d0f3c4e1f5c9d08cc2f8c1
manuel-gaviria <72875707+manuel-gaviria@users.noreply.github.com>
2021-10-17 10:21:19 -0500

    agregando archivos de jugadoresvictorgaviria

---
+++ src/app/components/jugador/jugador.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-jugador',
+  templateUrl: './jugador.component.html',
+  styleUrls: ['./jugador.component.css']
+})
+export class JugadorComponent implements OnInit {
+
+ 
+
+  constructor() { }
+  ngOnInit(): void {
+  
+}
+}
